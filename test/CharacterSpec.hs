6e2f83defbf072bbc1f95856e6c0ee71a49e73b3
Sendy Halim <sendyhalim93@gmail.com>
2017-11-04 14:44:23 +0700

    Setup hspec discovery

--- test/CharacterSpec.hs
+++ test/CharacterSpec.hs
@@ -1,9 +1,13 @@
-import qualified Character             as C
+module CharacterSpec (main, spec) where
+
+import qualified Character  as C
 import           Test.Hspec
-import           Test.Hspec.QuickCheck
 
 main :: IO ()
-main = hspec $ do
+main = hspec spec
+
+spec :: Spec
+spec = do
   describe "Character.isEscaped" $ do
     context "Given character \\t" $ do
       it "should return True" $ do
