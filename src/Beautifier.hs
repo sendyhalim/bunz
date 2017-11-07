2f4608b45fcaecf33d26e7a9da18d7a10f19dcb4
Sendy Halim <sendyhalim93@gmail.com>
2017-11-08 01:08:48 +0700

    Add function to extract first string occurence

--- src/Beautifier.hs
+++ src/Beautifier.hs
@@ -4,6 +4,7 @@
 module Beautifier
   ( beautify
   , indent
+  , firstString
   ) where
 
 import           Data.Monoid ((<>))
@@ -23,6 +24,16 @@ indent level str = T.replicate level indentation <> str
 trimmedTail :: T.Text -> T.Text
 trimmedTail = T.stripStart . T.tail
 
+string :: Bool -> T.Text -> T.Text
+string False str@(T.head -> '\"') = "\"" <> string True (T.tail str)
+-- TODO: Take care of escaped double quote e.g. "\"test\""
+string True str@(T.head -> '"') = "\""
+string True str = (T.pack [T.head str]) <> string True (T.tail str)
+
+-- | Extract the first string value starting from the front
+firstString :: T.Text -> T.Text
+firstString str = string False str
+
 beautify :: IndentationLevel -> T.Text -> T.Text
 beautify _ "" = ""
 beautify _ str@(T.length -> 0) = str
@@ -31,4 +42,4 @@ beautify i str@(T.head -> '\n') = beautify i (T.stripStart str)
 beautify i str@(T.head -> '\t') = beautify i (T.stripStart str)
 beautify i str@(T.head -> '{') = "{" <> newline <> beautify (i + 1) (trimmedTail str)
 beautify i str@(T.head -> '[') = "[" <> newline <> beautify (i + 1) (trimmedTail str)
-{- TODO: Add more pattern match -}
+-- TODO: Add more pattern match
