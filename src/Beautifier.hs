7c7dd4fc2f30644d619ec772a4d995b26a836803
Sendy Halim <sendyhalim93@gmail.com>
2017-11-07 23:10:59 +0700

    Add trimmedTail and newline functions

--- src/Beautifier.hs
+++ src/Beautifier.hs
@@ -14,11 +14,21 @@ type IndentationLevel = Int
 indentation :: T.Text
 indentation = "  "
 
+newline :: T.Text
+newline = "\n"
+
 indent :: IndentationLevel -> T.Text -> T.Text
 indent level str = T.replicate level indentation <> str
+
+trimmedTail :: T.Text -> T.Text
+trimmedTail = T.stripStart . T.tail
 
 beautify :: IndentationLevel -> T.Text -> T.Text
 beautify _ "" = ""
 beautify _ str@(T.length -> 0) = str
 beautify i str@(T.head -> ' ') = beautify i (T.stripStart str)
+beautify i str@(T.head -> '\n') = beautify i (T.stripStart str)
+beautify i str@(T.head -> '\t') = beautify i (T.stripStart str)
+beautify i str@(T.head -> '{') = "{" <> newline <> beautify (i + 1) (trimmedTail str)
+beautify i str@(T.head -> '[') = "[" <> newline <> beautify (i + 1) (trimmedTail str)
 {- TODO: Add more pattern match -}
