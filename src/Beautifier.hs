95cba2294bf10683775bd8e794b8b48a2b12fb38
Sendy Halim <sendyhalim93@gmail.com>
2017-11-29 00:30:34 +0700

    Add docs and doctest

--- src/Beautifier.hs
+++ src/Beautifier.hs
@@ -11,6 +11,10 @@ module Beautifier
 import           Data.Monoid ((<>))
 import qualified Data.Text   as T
 
+-- Doctest setup
+-- $setup
+-- >>> :set -XOverloadedStrings
+
 type IndentationLevel = Int
 
 indentation :: T.Text
@@ -25,12 +29,26 @@ indent level str = T.replicate level indentation <> str
 trimmedTail :: T.Text -> T.Text
 trimmedTail = T.stripStart . T.tail
 
--- | Split the given text at its head.
+-- |Split the given string at its head while honouring escaped string.
+-- Examples:
+--
+-- >>> splitAtHead "whatsup"
+-- ("w","hatsup")
+--
+-- >>> splitAtHead "\nyeay"
+-- ("\n","yeay")
 splitAtHead :: T.Text -> (T.Text, T.Text)
 splitAtHead "" = ("", "")
 splitAtHead str@(T.head -> '\\') = T.splitAt 2 str
 splitAtHead str = T.splitAt 1 str
 
+-- |Extract string within double quotes `"` including the double quotes ignoring
+-- the suffix after closing quote `"`
+--
+-- Examples:
+--
+-- >>> string False "\"test\"whatuppp"
+-- "\"test\""
 string :: Bool -> T.Text -> T.Text
 string False str@(T.head -> '"') = "\"" <> string True (T.tail str)
 string True str@(T.head -> '"') = "\""
