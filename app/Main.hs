d216b6b08a14b58ae0f7cb54e487d967e21c7a00
Sendy Halim <sendyhalim93@gmail.com>
2017-12-06 03:01:34 +0700

    Pass cmd arg monad to run function

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
+import qualified Beautifier             as B
+import           System.Console.CmdArgs ((&=))
+import qualified System.Console.CmdArgs as CA
+
+data Args = Args { jsonString :: String }
+  deriving (Show, CA.Data, CA.Typeable)
+
+
+args = Args { jsonString = CA.def &= CA.typ "JSON String" &= CA.argPos 0 }
+    &= CA.summary "JSON beautifier tool version 0.0.1"
+
 main :: IO ()
-main = putStrLn "Not implemented yet"
+main = CA.cmdArgs args >>= run
+
+run :: Args -> IO ()
+run (Args { jsonString = str }) = do
+  putStrLn (B.beautify str)
