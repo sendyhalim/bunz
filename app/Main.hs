b56a0951f06348a651245951ee0f455706955ba2
Sendy Halim <sendyhalim93@gmail.com>
2017-12-07 02:30:41 +0700

    Make the main function to be able to read from stdin

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,8 @@ module Main where
 import qualified Beautifier             as B
 import           System.Console.CmdArgs ((&=))
 import qualified System.Console.CmdArgs as CA
+import qualified System.Posix.IO        as IO
+import qualified System.Posix.Terminal  as T
 
 data Args = Args { jsonString :: String }
   deriving (Show, CA.Data, CA.Typeable)
@@ -14,8 +16,17 @@ args = Args { jsonString = CA.def &= CA.typ "JSON String" &= CA.argPos 0 }
     &= CA.summary "JSON beautifier tool version 0.0.1"
 
 main :: IO ()
-main = CA.cmdArgs args >>= run
+main = T.queryTerminal IO.stdInput >>= run
 
-run :: Args -> IO ()
-run (Args { jsonString = str }) = do
-  putStrLn (B.beautify str)
+printBeautified :: String -> IO ()
+printBeautified = putStrLn . B.beautify
+
+runFromStdInput :: IO ()
+runFromStdInput = getContents >>= printBeautified
+
+runFromArgs :: Args -> IO ()
+runFromArgs (Args { jsonString = str }) = printBeautified str
+
+run :: Bool -> IO ()
+run False = runFromStdInput
+run True = CA.cmdArgs args >>= runFromArgs
